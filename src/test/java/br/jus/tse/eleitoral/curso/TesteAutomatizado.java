04d26620597e2b30722a40db308a3f105261052c
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-08-17 22:30:52 -0300

    Incrementando o teste até a segunda página

--- src/test/java/br/jus/tse/eleitoral/curso/TesteAutomatizado.java
+++ src/test/java/br/jus/tse/eleitoral/curso/TesteAutomatizado.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteAutomatizado {
 	public static void main(String[] args) {
@@ -13,12 +14,24 @@ public class TesteAutomatizado {
 				//new FirefoxDriver();
 				//new SafariDriver();
 				new ChromeDriver();
-
-		driver.get("http://apps.tse.jus.br/titulonet2/paginas/requerimentoTitulo/mensagemInicial.faces");
-
-		WebElement botaoProximo = driver
+		
+		String paginaInicial = "http://apps.tse.jus.br/titulonet2/paginas/requerimentoTitulo/mensagemInicial.faces";
+		System.out.println("Página 1 [" + paginaInicial + "]");
+		driver.get(paginaInicial);
+		WebElement element = driver
 				.findElement(By.id("RequerimentoAtualizacaoConfirmacaoRequerimentoForm:j_idt26"));
+		element.click();
+		
+		System.out.println("Página 2 [" + driver.getCurrentUrl() + "]");
+		Select tituloInput = new Select(driver.findElement(By.id("form:titulo_input")));
+		tituloInput.selectByValue("TENHO_E_SEI");
+		driver.findElement(By.id("form:numero")).sendKeys("011983892020");
+		driver.findElement(By.id("form:nome")).sendKeys("Luciano Soares Bohnert");
+		driver.findElement(By.id("form:dataNascimento")).sendKeys("//////////17/02/1978");
+		driver.findElement(By.id("form:nomeMae")).sendKeys("Elizabeth Soares Bohnert");
+		driver.findElement(By.id("form:nomePai")).sendKeys("Alcino José Bohnert");
+		driver.findElement(By.id("form:confirmar")).click();
 
-		botaoProximo.click();
+		//driver.close();
 	}
 }

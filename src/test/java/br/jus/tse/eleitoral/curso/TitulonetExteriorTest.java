2e9d53af80cb33030fdfb602f8be55ddd8e00cc5
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-08-17 22:33:18 -0300

    Melhorando o nome da Classe de Teste

--- src/test/java/br/jus/tse/eleitoral/curso/TitulonetExteriorTest.java
+++ src/test/java/br/jus/tse/eleitoral/curso/TitulonetExteriorTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class TesteAutomatizado {
+public class TitulonetExteriorTest {
 	public static void main(String[] args) {
 		//System.setProperty("webdriver.firefox.bin", "/Applications/Firefox");
 

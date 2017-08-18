30db31c75b686f745f4775f1c055863b8b680be9
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-08-17 22:49:22 -0300

    Teste para Certidao Negativa

---
+++ src/test/java/br/jus/tse/eleitoral/curso/CertidaoNegativaAlistamentoEleitoralTest.java
@@ -0,0 +1,37 @@
+package br.jus.tse.eleitoral.curso;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CertidaoNegativaAlistamentoEleitoralTest {
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.firefox.bin", "/Applications/Firefox");
+
+		WebDriver driver = 
+				//new FirefoxDriver();
+				//new SafariDriver();
+				new ChromeDriver();
+		
+		String paginaInicial = "http://apps.tse.jus.br/saae/indexCertidaoNegativa.do";
+		System.out.println("Página 1 [" + paginaInicial + "]");
+		driver.get(paginaInicial);
+		WebElement element = driver
+				.findElement(By.name("eulicheckbox"));
+		element.click();
+		driver.findElement(By.name("Emissão de certidão")).click();
+		
+		System.out.println("Página 2 [" + driver.getCurrentUrl() + "]");
+		driver.findElement(By.name("nomeEleitor")).sendKeys("Luciano Soares Bohnert");
+		element = driver.findElement(By.name("dataNascimento"));
+		element.sendKeys("17/02/1978");
+		element.sendKeys(Keys.TAB);
+		driver.findElement(By.name("nomeMae")).sendKeys("Elizabeth Soares Bohnert");
+		driver.findElement(By.name("nomePai")).sendKeys("Alcino José Bohnert");
+		driver.findElement(By.id("consultar")).click();
+
+		//driver.close();
+	}
+}

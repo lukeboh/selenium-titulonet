a1d6e97140d8a83b9597aba862616583b04a5d62
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-08-19 22:45:05 -0300

    Refactoring para preparar para criar PageObject

--- src/test/java/br/jus/tse/eleitoral/curso/CertidaoNegativaAlistamentoEleitoralTest.java
+++ src/test/java/br/jus/tse/eleitoral/curso/CertidaoNegativaAlistamentoEleitoralTest.java
@@ -10,11 +10,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CertidaoNegativaAlistamentoEleitoralTest {
 
+	private static final String MSG_ELEITOR_CADASTRADO = "CONSTA registro de inscrição perante a Justiça Eleitoral para os dados informados.";
+	private WebDriver driver;
+
 	@Test
 	public void caminhoFeliz() {
+		driver = new ChromeDriver();
+		visita();
+		preencheDadosCertidao("Luciano Soares Bohnert", "17/02/1978", "Elizabeth Soares Bohnert",
+				"Alcino José Bohnert");
+		String retorno = submeteEmissaoCertidao();
+		Assert.assertNotNull("Está emitindo certidão negativa de alistamento para eleitor cadastrado", retorno);
+		Assert.assertTrue("Mensagem incorreta", retorno.contains(MSG_ELEITOR_CADASTRADO));
+		// driver.close();
+	}
 
-		WebDriver driver = new ChromeDriver();
-
+	public void visita() {
 		String paginaInicial = "http://apps.tse.jus.br/saae/indexCertidaoNegativa.do";
 		System.out.println("Página 1 [" + paginaInicial + "]");
 		driver.get(paginaInicial);
@@ -23,22 +34,30 @@ public class CertidaoNegativaAlistamentoEleitoralTest {
 		driver.findElement(By.name("Emissão de certidão")).click();
 
 		System.out.println("Página 2 [" + driver.getCurrentUrl() + "]");
-		driver.findElement(By.name("nomeEleitor")).sendKeys("Luciano Soares Bohnert");
+	}
+
+	public void preencheDadosCertidao(String nomeEleitor, String dataNascimentoEleitor, String nomeMaeEleitor,
+			String nomePaiEleitor) {
+		WebElement element;
+		driver.findElement(By.name("nomeEleitor")).sendKeys(nomeEleitor);
 		element = driver.findElement(By.name("dataNascimento"));
-		element.sendKeys("17/02/1978");
+		element.sendKeys(dataNascimentoEleitor);
 		element.sendKeys(Keys.TAB);
-		driver.findElement(By.name("nomeMae")).sendKeys("Elizabeth Soares Bohnert");
-		driver.findElement(By.name("nomePai")).sendKeys("Alcino José Bohnert");
+		driver.findElement(By.name("nomeMae")).sendKeys(nomeMaeEleitor);
+		driver.findElement(By.name("nomePai")).sendKeys(nomePaiEleitor);
+	}
+
+	/**
+	 * @return <code>null</code> se emitiu certidão com sucesso, ou a mensagem
+	 *         de erro exibida.
+	 */
+	public String submeteEmissaoCertidao() {
 		driver.findElement(By.id("consultar")).submit();
-		
-		if (driver.findElement(By.className("g-recaptcha")) == null) {
-			element = driver.findElement(By.name("mensagens_anchor"));
-			Assert.assertTrue("Está emitindo certidão negativa de alistamento para eleitor cadastrado", element.getText()
-					.contains("CONSTA registro de inscrição perante a Justiça Eleitoral para os dados informados."));
-		} else {
-			Assert.fail("Captcha está habilitado e não se pode testar funcionalidade");
+		WebElement element;
+		element = driver.findElement(By.name("mensagens_anchor"));
+		if (!"".equals(element.getText())) {
+			return element.getText();
 		}
-			
-		// driver.close();
+		return null;
 	}
 }
